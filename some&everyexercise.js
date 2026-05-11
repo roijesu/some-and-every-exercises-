8522652a6ed0fc884d20a93a9cd6087beeef03ef
roijesu <135164728+roijesu@users.noreply.github.com>
2026-05-10 20:32:37 -0400

    Add files via upload

---
+++ some&everyexercise.js
@@ -0,0 +1,36 @@
+function hasOddNumber(arr){
+    return arr.some(function(val){
+       return val % 2 !== 0;
+        });
+}
+
+function hasAZero(num){
+    return num.toString().split('').some(function(val){
+        return  val === '0' ;
+    });
+}
+
+function hasOnlyOddNumbers(arr){
+    return arr.every(function(val){
+      return  val % 2 !== 0;
+        });
+}
+
+function hasNoDuplicates(arr){
+   return arr.every(function(val){
+    return arr.indexOf(val) === arr.lastIndexOf(val);
+  });
+}
+
+function hasCertainKey(arr, key){
+    return arr.every(function(val){
+        return val in key
+    });
+}
+
+function hasCertainValue(arr, key, searchVal){
+    return arr.every(function(val){
+        return val[key] === searchVal ;
+    });
+}
+
